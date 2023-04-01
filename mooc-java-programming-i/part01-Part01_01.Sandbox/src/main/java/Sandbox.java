2bd290b39ae8d73ddf5f9a2e808f45844e84c5fe
Theo Higgins <theo.higgins@Theos-MacBook-Pro.local>
2023-04-01 13:08:40 -0400

    remove deleted files

--- mooc-java-programming-i/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++
@@ -1,9 +0,0 @@
-
-public class Sandbox {
-
-    public static void main(String[] args) {
-        // Write your program here
-        
-        System.out.println("I already know how to do this. I will do it again though.");
-    }
-}
